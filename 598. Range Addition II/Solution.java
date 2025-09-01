5e60df30c6108a21f9a87eb20d9c15790c3b758d
Shailesh Adole <adoleshailesh2@gmail.com>
2025-09-01 16:33:16 +0530

    Leetcode 598

---
+++ 598. Range Addition II/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+
+        if(ops.length == 0) return m * n;
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+
+        for(int i = 0; i < ops.length; i++){
+            minX = Math.min(minX, ops[i][0]);
+            minY = Math.min(minY, ops[i][1]);
+        }
+
+        System.out.println(minX * minY);
+        return minX * minY;
+    }
+}
